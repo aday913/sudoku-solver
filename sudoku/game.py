346b10fb541236c7e29d0e9368668820a57a459f
Alex Day <alexdaygjj@gmail.com>
2023-04-14 01:40:01 -0700

    Initialize main py files

---
+++ sudoku/game.py
@@ -0,0 +1,15 @@
+board_vals = [
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None],
+    [None, None, None, None, None, None, None, None, None]
+]
+
+class Sudoku():
+    def __init__(self, start_state, player) -> None:
+        pass
