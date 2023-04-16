6758ec4afd392bac82d4880354439a708469456b
Alex Day <alexdaygjj@gmail.com>
2023-04-16 02:56:31 -0700

    Initialize game class

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ import argparse
 from sudoku.game import SudokuManager
 from sudoku.players import SudokuPlayer, ManualPlayer, BruteForcePlayer
 
+class SudokuGame():
+    def __init__(self, 
+                    player : SudokuPlayer, 
+                    board_file : str,
+                ):
+        self.manager = SudokuManager(board_file)
+        self.player  = player
+    
+    def get_next_move(self):
+        pass
+
+    def check_if_won(self):
+        pass
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
@@ -13,4 +27,3 @@ if __name__ == '__main__':
 
     sudoku = SudokuManager(args.board)
     print(sudoku.get_print_board())
-    print(sudoku.is_valid_board(sudoku.board_vals))
